2342f562d27b2abecd2e2231caf41c652882f8ca
Eri Schon <erischon@gmail.com>
2023-07-31 08:43:52 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex w-full h-screen flex-col items-center justify-center">
-      <h1 className="text-2xl font-semibold">The Divalch Project</h1>
+      <h1 className="text-2xl font-semibold">The Dizalch Project</h1>
     </main>
   );
 }
