b4a9b00b74dbd67ea41942705be075eab9a89fdb
Eri Schon <erischon@gmail.com>
2023-08-05 10:46:53 +0200

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,31 @@
+import Link from "next/link";
+
+import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
+
+/**
+ * @description Footer component
+ * @version 1.0.0
+ */
 const Footer = () => {
   return (
-    <div className="text-gray-300 text-sm grid grid-cols-12 p-4">
-      <div className="col-span-4"> Col 1</div>
-      <div className="col-span-4"> Col 2</div>
-      <div className="col-span-4"> Col 3</div>
+    <div className="flex flex-col-reverse gap-4 py-8 items-end sm:flex-row sm:justify-between sm:items-center">
+      <div className="text-gray-300 text-xs">
+        © 2020-2023 Eri Schön All rights reserved
+      </div>
+
+      <div className="flex gap-2">
+        <Link href="https://www.linkedin.com/in/ericschonhofen/">
+          <BsLinkedin className="text-3xl inline-block mr-2 text-teal-300 hover:text-gray-300" />
+        </Link>
+
+        <Link href="https://github.com/erischon">
+          <BsGithub className="text-3xl inline-block mr-2 text-teal-300 hover:text-gray-300" />
+        </Link>
+
+        <Link href="https://twitter.com/erischon">
+          <BsTwitter className="text-3xl inline-block mr-2 text-teal-300 hover:text-gray-300" />
+        </Link>
+      </div>
     </div>
   );
 };
