c5ecf44a2885db1f7a95eb36ac0a1403d792afea
Eri Schon <erischon@gmail.com>
2023-08-18 12:56:50 +0200

    Update ServiceCard.tsx

--- src/components/portfolio/ServiceCard.tsx
+++ src/components/portfolio/ServiceCard.tsx
@@ -17,20 +17,17 @@ const ServiceCard = ({ service }: { service: ServiceCardProps }) => {
   return (
     <>
       <div className="group h-60 w-60 [perspective:1000px] mx-auto">
-        <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] [backface-visibility:hidden]">
+        <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] [backface-visibility:hidden] bg-gray-700">
           <div className="absolute inset-0">
-            <Image
-              className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-              alt={title}
-              src={image}
-              fill
-            />
+            <div className="flex flex-col justify-center items-center h-full w-full">
+              <div className="text-4xl text-teal-400 mb-4">{icon}</div>
+              <h2 className="text-2xl font-medium">{title}</h2>
+            </div>
           </div>
 
           <div className="absolute inset-0 h-full w-full rounded-xl bg-gray-700 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
             <div className="flex min-h-full flex-col items-center justify-center">
-              <h2 className="text-xl font-semibold mb-4">{title}</h2>
-              <p className="">{description}</p>
+              <p className="text-lg">{description}</p>
             </div>
           </div>
         </div>
