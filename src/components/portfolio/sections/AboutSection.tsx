dafaf647cf73d493fea864c9938b54d8a294115a
Eri Schon <erischon@gmail.com>
2023-08-19 18:58:57 +0200

    Update AboutSection.tsx

--- src/components/portfolio/sections/AboutSection.tsx
+++ src/components/portfolio/sections/AboutSection.tsx
@@ -11,7 +11,7 @@ const AboutSection = () => {
       className="min-h-screen pt-[4rem] lg:pt-[6rem] px-[6%] pb-[2rem] lg:pb-[4rem] bg-indigo-50 dark:bg-indigo-900"
       id="about"
     >
-      <div className="grid md:grid-cols-12 mx-auto">
+      <div className="grid md:grid-cols-12 mx-auto mb-8">
         <div className="md:col-span-6 order-2 md:order-1">
           <div className="flex flex-col items-start mb-8">
             <p className="mb-3 text-indigo-500 dark:text-indigo-300 relative after:inline-block after:w-20 after:h-[0.1rem] after:bg-indigo-300 after:bottom-[50%] after:right-[-6rem] after:absolute">
@@ -45,27 +45,27 @@ const AboutSection = () => {
           </div>
         </div>
 
-        <div className="md:col-span-6 mx-auto order-1 md:order-2">
+        <div className="md:col-span-6 mx-auto my-auto order-1 md:order-2">
           <div className="w-80 h-80  relative">
             <Image src="/erischon_indigo.png" alt="Eri Schön" fill />
           </div>
         </div>
       </div>
 
-      <div className="grid bg-indigo-700 w-full md:grid-cols-12 rounded-lg p-4">
-        <div className="flex flex-col items-center px-8 md:px-28 py-10 text-gray-100 md:col-span-4 ">
+      <div className="flex w-full flex-wrap justify-center">
+        <div className="flex flex-col items-center px-8 md:px-28 py-10 text-gray-100 bg-indigo-700 rounded-lg shadow-md dark:shadow-black/60 m-4 min-w-[200px]">
           <AiOutlineCheckCircle className="text-4xl mb-2" />
           <p className="text-4xl font-bold mb-0">120</p>
           <p>projects</p>
         </div>
 
-        <div className="flex flex-col items-center px-28 py-10 text-gray-100 md:col-span-4 border-b-4 border-t-4 md:border-t-0 md:border-b-0 md:border-l-4 md:border-r-4 border-gray-100">
+        <div className="flex flex-col items-center px-8 md:px-28 py-10 text-gray-100 bg-indigo-700 rounded-lg shadow-md dark:shadow-black/60 m-4 min-w-[200px]">
           <AiOutlineCheckCircle className="text-4xl mb-2" />
           <p className="text-4xl font-bold mb-0">120</p>
           <p>projects</p>
         </div>
 
-        <div className="flex flex-col items-center px-28 py-10 text-gray-100 md:col-span-4">
+        <div className="flex flex-col items-center px-8 md:px-28 py-10 text-gray-100 bg-indigo-700 rounded-lg shadow-md dark:shadow-black/60 m-4 min-w-[200px]">
           <AiOutlineCheckCircle className="text-4xl mb-2" />
           <p className="text-4xl font-bold mb-0">120</p>
           <p>projects</p>
