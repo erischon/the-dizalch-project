0d72d46886a6b3422bd47649d0005b8e2b5c621d
Eri Schon <erischon@gmail.com>
2023-08-21 17:19:55 +0200

    hero

--- src/components/public/sections/HeroSection.tsx
+++ src/components/public/sections/HeroSection.tsx
@@ -7,20 +7,13 @@ const HeroSection = () => {
   return (
     <>
       <section
-        className="min-h-[calc(100vh-70px)] lg:pt-[6rem] px-[6%] lg:pb-[4rem] bg-indigo-50 dark:bg-indigo-900"
+        className="min-h-[calc(100vh-70px)] lg:py-[4rem] px-[6%] bg-indigo-500 dark:bg-indigo-900"
         id="hero"
       >
-        <div className="grid lg:grid-cols-12 lg:items-center">
-          <div className="max-w-screen-2xl lg:col-span-7 mr-auto place-self-center order-2 lg:order-1 text-center lg:text-left">
-            <h1 className="mb-8 text-3xl md:text-4xl lg:text-6xl font-extrabold lg:leading-tight sm:max-w-lg mx-auto lg:mx-0">
-              La ressource{" "}
-              <span className="uppercase text-indigo-500 dark:text-indigo-300">
-                Qualité
-              </span>{" "}
-              de votre appli web{" "}
-              <span className="uppercase text-indigo-500 dark:text-indigo-300">
-                React
-              </span>
+        <div className="grid lg:grid-cols-12 lg:items-center rounded-lg shadow-lg">
+          <div className="bg-red-500 max-w-screen-2xl h-full lg:col-span-6 mr-auto place-self-center order-2 lg:order-1 text-center lg:text-left ">
+            <h1 className="mb-8 text-3xl md:text-4xl lg:text-6xl font-extrabold lg:leading-tight sm:max-w-lg mx-auto lg:mx-0 text-gray-100">
+              Optimiser votre application React
             </h1>
 
             <h3 className="mb-10 lg:text-lg">
@@ -30,15 +23,15 @@ const HeroSection = () => {
             </h3>
 
             <a href="mailto:erischondev@gmail.com" className="">
-              <button className="bg-indigo-500 text-gray-100 font-semibold px-9 py-3 hover:bg-transparent hover:text-indigo-600 border border-indigo-600 dark:hover:text-gray-100 dark:hover:border-gray-100 text-lg md:text-2xl transition duration-300">
+              <button className="bg-gray-100 text-gray-700 font-semibold px-9 py-3 hover:bg-transparent hover:text-gray-100 border border-gray-100 dark:hover:text-gray-100 dark:hover:border-gray-100 text-lg md:text-2xl transition duration-300 rounded-lg">
                 Parlons-en !
               </button>
             </a>
           </div>
 
-          <div className="relative lg:flex lg:col-span-5 lg:mt-0 w-80 h-80 order-1 lg:order-2 mx-auto">
+          <div className="relative lg:flex lg:col-span-6 lg:mb-0 w-[17rem] h-[20rem] order-1 lg:order-2 mx-auto">
             <Image
-              src="/images/hero-5.png"
+              src="/images/hero-10.png"
               alt="hero"
               fill
               className="object-contain"
