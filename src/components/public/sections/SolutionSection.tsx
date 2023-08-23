9af727ad8492cb24cddb725a804476d137e3a756
Eri Schon <erischon@gmail.com>
2023-08-23 12:45:20 +0200

    Update SolutionSection.tsx

--- src/components/public/sections/SolutionSection.tsx
+++ src/components/public/sections/SolutionSection.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
+import { gains } from "@/data/infos";
+import GainCard from "../GainCard";
+
 import {
   slideRightToLeftVariants,
   slideLeftToRightVariants,
@@ -22,44 +25,33 @@ export default function SolutionSection() {
         <p className="text-md text-gray-500 mb-5">La solution</p>
 
         <h2 className="text-4xl text-gray-800 font-semibold">
-          Réduisez votre dette technique et optimisez votre process de
-          développement
+          Augmenter la Qualité et optimiser le process de développement
         </h2>
       </div>
 
       <div className="grid md:grid-cols-12 mb-10">
-        <motion.div
-          className="text-gray-800 md:col-span-7 md:pr-14 mb-5 max-w-3xl"
-          variants={slideLeftToRightVariants}
-          initial="initial"
-          whileInView="animate"
-          transition={{ ease: "easeIn", duration: 0.5 }}
-          viewport={{ once: true }}
-        >
-          <p className="md:text-lg lg:text-xl mb-8">
+        <div className="text-gray-800 md:col-span-7 md:pr-14 mb-5 max-w-3xl">
+          <motion.p
+            className="md:text-lg lg:text-xl mb-10"
+            variants={slideLeftToRightVariants}
+            initial="initial"
+            whileInView="animate"
+            transition={{ ease: "easeIn", duration: 0.5 }}
+            viewport={{ once: true }}
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex et
             consequuntur cum harum. Debitis veritatis quaerat dolor ratione
             voluptatibus repellat! Lorem ipsum dolor, sit amet consectetur
             adipisicing elit. Porro dolor ullam laborum doloribus maxime!
             Deleniti nesciunt nostrum corporis accusamus iure?
-          </p>
+          </motion.p>
 
-          <p className="md:text-lg lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex et
-            consequuntur cum harum. Debitis veritatis quaerat dolor ratione
-            voluptatibus repellat! Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Porro dolor ullam laborum doloribus maxime!
-            Deleniti nesciunt nostrum corporis accusamus iure?
-          </p>
-
-          <p className="md:text-lg lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex et
-            consequuntur cum harum. Debitis veritatis quaerat dolor ratione
-            voluptatibus repellat! Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Porro dolor ullam laborum doloribus maxime!
-            Deleniti nesciunt nostrum corporis accusamus iure?
-          </p>
-        </motion.div>
+          <div className="grid sm:grid-cols-12 sm:justify-center gap-4">
+            {gains.map((gain) => (
+              <GainCard gain={gain} key={gain.title} />
+            ))}
+          </div>
+        </div>
 
         <motion.div
           className="relative w-[20rem] lg:w-[25rem] h-[20rem] lg:h-[25rem] mx-auto mt-4 md:my-auto md:col-span-5"
