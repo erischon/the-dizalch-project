e23eac48f6aafa653d9e235a5161c4788708a26c
Eri Schon <erischon@gmail.com>
2023-08-24 15:54:21 +0200

    services

--- src/components/public/sections/ServicesSection.tsx
+++ src/components/public/sections/ServicesSection.tsx
@@ -1,29 +1,53 @@
+"use client";
+
+import Image from "next/image";
+import { useMediaQuery } from "usehooks-ts";
+
 import { services } from "@/data/infos";
 
 import ServiceCard from "@/components/public//ServiceCard";
 import Section from "@/components/public/Section";
+import Slideshow from "@/components/public/Slideshow";
 
 /**
  * @description Services Section component,
  * @version 1.0.0
  */
 export default function ServicesSection() {
+  const media = useMediaQuery("(max-width: 768px)");
+
   return (
     <Section id="services">
-      <div className="flex flex-col items-center mb-8">
-        <p className="mb-2 text-indigo-500 dark:text-indigo-300 relative before:inline-block before:w-20 before:h-[0.1rem] before:bg-indigo-300 before:bottom-[50%] before:left-[-6rem] before:absolute after:inline-block after:w-20 after:h-[0.1rem] after:bg-indigo-300 after:bottom-[50%] after:right-[-6rem] after:absolute">
-          Nos services
+      <div className="flex flex-col md:items-center mb-10 text-center md:text-left w-full">
+        <p className="text-md text-gray-300 mb-5">
+          Ce que nous faisons pour vous
         </p>
 
-        <h2 className="text-3xl font-semibold text-gray-900">
-          Ce que nous faisons pour vous
+        <h2 className="text-4xl text-gray-00 font-semibold max-w-3xl text-center leading-snug">
+          Augmenter la Qualité et optimiser le process de développement
         </h2>
       </div>
 
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 justify-center gap-4">
-        {services.map((service) => (
-          <ServiceCard service={service} key={service.title} />
-        ))}
+      <div className="grid lg:grid-cols-12 grow">
+        <div className="relative w-[20rem] h-[20rem] lg:w-[25rem] lg:h-[25rem] mx-auto lg:col-span-5 my-10 md:my-0">
+          <Image src="/images/services.png" alt="" fill className="" />
+        </div>
+
+        <div className="lg:col-span-7 grid md:grid-cols-3 lg:grid-cols-3 md:gap-5">
+          {!media ? (
+            <>
+              {services.map((service) => (
+                <ServiceCard service={service} key={service.title} />
+              ))}
+            </>
+          ) : (
+            <Slideshow
+              content={services.map((service) => (
+                <ServiceCard service={service} key={service.title} />
+              ))}
+            />
+          )}
+        </div>
       </div>
     </Section>
   );
--- src/data/infos.js
+++ src/data/infos.js
@@ -32,43 +32,37 @@ export const projectsListPageSize = 6;
 export const services = [
   {
     title: "Testing",
-    description:
-      "Nous mettons en place des tests unitaires ainsi que des tests end to end afin que votre application web soit robuste et fiable",
+    description: "Tests fonctionnels et E2E",
     icon: <GiTestTubes />,
     image: "/images/services/testing.jpg",
   },
   {
     title: "Documentation",
-    description:
-      "Nous documentons votre application web afin que vous puissiez la maintenir et la faire évoluer facilement.",
+    description: "Documentation du code",
     icon: <HiOutlineDocumentText />,
     image: "/images/services/security.jpg",
   },
   {
     title: "Performance",
-    description:
-      "Nous annalysons, optimisons et améliorons les performances de votre application web pour qu'elle soit rapide et efficace.",
+    description: "Analyse et optimisation",
     icon: <BsGraphUp />,
     image: "/images/services/performance.jpg",
   },
   {
-    title: "Maintenance",
-    description:
-      "Nous effectuons la maintenance de votre application web ainsi que les mises à jour nécessaires pour qu'elle reste fonctionnelle.",
+    title: "Mise à jour",
+    description: "Mise à jour des dépendances",
     icon: <FaTools />,
     image: "/images/services/security.jpg",
   },
   {
     title: "Debugging",
-    description:
-      "Nous aidons à résoudre les bugs pour que votre application web fonctionne correctement et sans erreurs.",
+    description: "Correction de bugs",
     icon: <BsFillBugFill />,
     image: "/images/services/security.jpg",
   },
   {
     title: "Sécurité",
-    description:
-      "Nous sécurisons votre application web grâce à des tests de sécurité et des audits afin de vous protéger contre les attaques malveillantes.",
+    description: "Analyse et correction",
     icon: <MdSecurity />,
     image: "/images/services/security.jpg",
   },
