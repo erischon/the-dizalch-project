09e1316f37eb93858005a02aa3662a45e0956901
Eri Schon <erischon@gmail.com>
2023-08-24 12:11:26 +0200

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import { Metadata } from "next";
-import { motion } from "framer-motion";
 
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
@@ -45,12 +42,6 @@ export default function PortfolioLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const variants = {
-    hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -100 },
-  };
-
   return (
     <div className="flex flex-col w-full justify-between">
       <header className="px-4 lg:px-8 flex justify-between items-center min-h-[70px]">
@@ -61,17 +52,7 @@ export default function PortfolioLayout({
         </div>
       </header>
 
-      <motion.main
-        initial="hidden"
-        animate="enter"
-        exit="exit"
-        variants={variants}
-        transition={{ type: "linear" }}
-      >
-        {children}
-      </motion.main>
-
-      {/* <main className="">{children}</main> */}
+      <main className="">{children}</main>
 
       <footer className="px-4 lg:px-8 bg-gray-900">
         <Footer />
