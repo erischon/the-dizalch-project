ef2a4b57797ece25fac16ad61b3337458f9eaebd
erischon <66017574+erischon@users.noreply.github.com>
2023-08-26 09:29:46 +0200

    solution refactoring

--- src/components/public/sections/SolutionSection.tsx
+++ src/components/public/sections/SolutionSection.tsx
@@ -23,46 +23,46 @@ export default function SolutionSection() {
   return (
     <Section id="solution" background="bg-gray-800">
       <Container background="bg-gray-900">
-        <SectionHeader header={header} title={title} style="center" />
+        <SectionHeader header={header} title={title} />
 
-        <div className="grid lg:grid-cols-12 grow">
-          <div className="lg:col-span-7 lg:pr-14 max-w-3xl flex flex-col md:mb-10 lg:mb-0">
-            <div className="mb-5 lg:mb-14">
-              <p className="text-center md:text-left md:text-lg lg:text-2xl mb-5">
+        <div className="grid lg:grid-cols-12 grow md:mb-10">
+          <div className="lg:col-span-7 lg:pr-14 max-w-3xl flex flex-col">
+            <div className="flex flex-col gap-5 md:text-lg lg:text-xl mb-5">
+              <p className="">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
                 praesentium! Quo voluptates tenetur fugit deserunt reprehenderit
                 dolorem, adipisci neque perferendis.
               </p>
 
-              <p className="text-center md:text-left md:text-lg lg:text-2xl mb-5">
+              <p className="">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
                 praesentium! Quo voluptates tenetur fugit deserunt reprehenderit
                 dolorem, adipisci neque perferendis.
               </p>
-            </div>
-
-            <div className="grid md:grid-cols-3 lg:grid-cols-12 sm:justify-center gap-4 grow items-center">
-              {!media ? (
-                <>
-                  {gains.map((gain, index) => (
-                    <div className="lg:col-span-4" key={index}>
-                      <GainCard gain={gain} />
-                    </div>
-                  ))}
-                </>
-              ) : (
-                <Slideshow
-                  content={gains.map((gain) => (
-                    <GainCard gain={gain} key={gain.title} />
-                  ))}
-                />
-              )}
             </div>
           </div>
 
-          <div className="relative w-[20rem] h-[20rem] lg:w-[25rem] lg:h-[25rem] mx-auto my-auto lg:col-span-5 hidden md:block">
+          <div className="relative w-[14rem] h-[14rem] lg:w-[22rem] lg:h-[22rem] mx-auto lg:col-span-5 hidden md:block">
             <Image src="/images/solution.png" alt="" fill className="" />
           </div>
+        </div>
+
+        <div className="grid md:grid-cols-12 sm:justify-center gap-4 grow items-center">
+          {!media ? (
+            <>
+              {gains.map((gain, index) => (
+                <div className="lg:col-span-4" key={index}>
+                  <GainCard gain={gain} />
+                </div>
+              ))}
+            </>
+          ) : (
+            <Slideshow
+              content={gains.map((gain) => (
+                <GainCard gain={gain} key={gain.title} />
+              ))}
+            />
+          )}
         </div>
       </Container>
     </Section>
