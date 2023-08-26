5be4aaa8ee2b020175c6b8420a2e60f4fff56e87
erischon <66017574+erischon@users.noreply.github.com>
2023-08-26 11:14:44 +0200

    Update GainCard.tsx

--- src/components/public/GainCard.tsx
+++ src/components/public/GainCard.tsx
@@ -13,7 +13,7 @@ export default function GainCard({ gain }: { gain: GainCardProps }) {
 
   return (
     <>
-      <div className="group min-w-fit mx-auto p-5 w-60 h-60 md:w-full md:h-fit rounded-lg shadow-md  bg-indigo-800 flex flex-col justify-center lg:col-span-4">
+      <div className="group min-w-fit mx-auto p-5 w-60 h-60 md:w-full  rounded-lg shadow-md  bg-indigo-800 flex flex-col justify-center lg:col-span-4">
         <div className="text-3xl mb-5 w-fit mx-auto text-indigo-900">
           <div className="p-4 bg-indigo-100 rounded-full">{icon}</div>
         </div>
